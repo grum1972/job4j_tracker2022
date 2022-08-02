a3d00dd1303897904718b01567b133769ff49e21
Kotov Oleg <grum1972@gmail.com>
2022-08-02 14:10:25 +0300

    2. Идентичные тексты [#212655 #203751]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String el : origin) {
+            check.add(el);
+        }
+        for (String el: text) {
+            if (!check.contains(el)) {
+                return false;
+            }
+        }
+        return rsl;
+    }
+}
